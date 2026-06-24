c7ec7f5528f640535b7da72c5804cd736c8d9872
Pizduk-Lenia <danil.tikhomirov.06@inbox.ru>
2026-06-24 04:36:09 +0300

    ансамбли(регрессия)

--- ensemble/__init__.py
+++ ensemble/__init__.py
@@ -1,1 +1,2 @@
 from ._classification import RandomForestClassifier
+from ._regression import RandomForestRegressor
---
+++ ensemble/_regression.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+from tree import DecisionTreeRegressor
+from sklearn.base import RegressorMixin, BaseEstimator
+from joblib import delayed, Parallel
+
+class RandomForestRegressor(RegressorMixin, BaseEstimator):
+    def __init__(self, n_estimators=100, bootstrap=True, max_samples=1, random_state=42,
+                 max_depth=10, min_samples_split=2, min_samples_leaf=1, max_features="sqrt", min_impurity_decrease=1e-12):
+        
+        self.n_estimators = n_estimators
+        self.bootstrap = bootstrap
+        self.max_samples = max_samples
+        self.random_state = random_state
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.max_features = max_features
+        self.min_impurity_decrease = min_impurity_decrease
+
+    def _converter_to_numpy(self, X):
+        if isinstance(X, pd.DataFrame) or isinstance(X, pd.Series):
+            return X.to_numpy()     
+        elif isinstance(X, np.ndarray):
+            return X
+        else:
+            return np.asarray(X)
+
+    def _fit_tree(self, X, y, tree_params, seed):
+        if self.bootstrap:
+            _rng = np.random.default_rng(seed)
+            bootstrap_index = _rng.choice(X.shape[0], int(X.shape[0] * self.max_samples), replace=True)
+            tree = DecisionTreeRegressor(**tree_params, random_state=seed)
+            tree.fit(X[bootstrap_index], y[bootstrap_index])
+            return tree
+        else:
+            tree = DecisionTreeRegressor(**tree_params, random_state=seed)
+            tree.fit(X, y)
+            return tree
+    
+    def _predict_by_tree(self, X, tree):
+        return tree.predict(X)
+
+    def fit(self, X, y):
+        X_ = self._converter_to_numpy(X)
+        y_ = self._converter_to_numpy(y)
+        seeds_list = []
+
+        for i in range(self.n_estimators):
+            seeds_list.append(self.random_state + i)
+
+        tree_params = {
+        "max_depth": self.max_depth,
+        "min_samples_split": self.min_samples_split,
+        "min_samples_leaf": self.min_samples_leaf,
+        "max_features": self.max_features,
+        "min_impurity_decrease": self.min_impurity_decrease,
+        }
+
+        self._trees = Parallel(n_jobs=-1)(delayed(self._fit_tree)(X_, y_, tree_params, s) for s in seeds_list)
+        return self
+    
+    def predict(self, X):
+        X_ = self._converter_to_numpy(X)
+        answers = Parallel(n_jobs=-1)(delayed(self._predict_by_tree)(X_, tree) for tree in self._trees)
+        answers = np.array(answers)
+        result = answers.mean(axis=0)
+        return result
